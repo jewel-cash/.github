d45a6332c0bdedd386b151abb6f5f17901c0803e
Wilhelm Thieme <wjthieme@gmail.com>
2022-08-10 14:13:22 +0200

    Firebase app check

--- packages/frontend/src/firebase.ts
+++ packages/frontend/src/firebase.ts
@@ -1,20 +1,28 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, onIdTokenChanged } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getPerformance } from "firebase/performance";
-import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
+import { initializeAppCheck, onTokenChanged, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 import { EnvKeyTransform, extractFromEnv } from "core";
-
-console.log(process.env);
 
 const firebaseKey = extractFromEnv("REACT_APP_FIREBASE", EnvKeyTransform.CamelCase);
 const app = initializeApp(firebaseKey);
 
-export const sginature = initializeAppCheck(app, {
+
+
+export const appCheck = initializeAppCheck(app, {
     provider: new ReCaptchaEnterpriseProvider(firebaseKey.captchaKey),
     isTokenAutoRefreshEnabled: true
 });
-
 export const analytics = getAnalytics(app);
 export const performance = getPerformance(app);
 export const auth = getAuth(app);
+
+onTokenChanged(appCheck, (result) => {
+    console.log(result.token)
+});
+
+onIdTokenChanged(auth, async (user) => {
+    const token = await user?.getIdTokenResult()
+    console.log(token);
+})
