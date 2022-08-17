8684e1e00bbbedd9fcfc0f3946601f8dcc3981f6
Wilhelm Thieme <wjthieme@gmail.com>
2022-08-17 17:08:03 +0200

    Cleanup

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import swaggerUi from "swagger-ui-express";
-import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import { RegisterRoutes } from "./modules/routes.gen.js";
@@ -20,7 +19,6 @@ const app: Application = express();
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static("./public"));
 
 RegisterRoutes(app);
