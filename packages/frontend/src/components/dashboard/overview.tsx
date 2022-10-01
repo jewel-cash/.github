47d06190909b53fff25ad67bd93c7a91b3c60ca5
Wilhelm Thieme <wjthieme@gmail.com>
2022-10-01 16:04:12 +0200

    Added tiles to the dashboard

---
+++ packages/frontend/src/components/dashboard/overview.tsx
@@ -0,0 +1,45 @@
+import "../../styles/dashboard/overview.css";
+import React, { Component } from "react";
+import { getDashboardOverview } from "../../modules/api";
+import { withAuth0, WithAuth0Props } from "@auth0/auth0-react";
+
+interface IState {
+    cumlative: string,
+    pending: string,
+    nextDate: string
+}
+
+class Overview extends Component<WithAuth0Props, IState> {
+    constructor(props: WithAuth0Props) {
+        super(props)
+        this.state = { 
+            cumlative: "? USD",
+            pending: "? USD",
+            nextDate: "?"
+        }
+    }
+    
+    componentDidMount() {
+        this.props.auth0.getAccessTokenSilently()
+            .then(getDashboardOverview)
+            .then(res => {
+                this.setState({
+                    cumlative: res.cumlative,
+                    pending: res.pending,
+                    nextDate: `${res.nextPaymentDate}`
+                })
+            }).catch(console.log)
+    }
+
+    render() {
+        return (
+            <div className="overview">
+                Cumlative: {this.state.cumlative}
+                Pending: {this.state.pending}
+                NextPayment: {this.state.nextDate}
+            </div>
+        );
+    }
+}
+
+export default withAuth0(Overview);
---
+++ packages/frontend/src/components/dashboard/stripe.tsx
@@ -0,0 +1,26 @@
+import "../../styles/dashboard/stripe.css";
+import React, { Component } from "react";
+
+interface IState {
+    setupCompleted: boolean
+}
+
+
+export default class Stripe extends Component<any, IState> {
+    constructor(props: any) {
+        super(props)
+        this.state = { setupCompleted: false }
+    }
+    
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div className="stripe">
+                Stripe
+            </div>
+        );
+    }
+}
