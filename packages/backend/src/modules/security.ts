7a7e5d3b4f333eb94d535baf7dfa90b2a3e84c3c
Wilhelm Thieme <wjthieme@gmail.com>
2022-11-02 10:31:08 +0100

    lint fix

--- packages/backend/src/modules/security.ts
+++ packages/backend/src/modules/security.ts
@@ -1,6 +1,6 @@
 import { Application } from "express";
 import cors from "cors";
-import slowDown from 'express-slow-down'
+import slowDown from "express-slow-down";
 
 export const RegisterSecurityMiddleware = (app: Application) => {
     app.use(cors());
