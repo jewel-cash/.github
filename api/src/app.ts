b9af50d1a26c6123b62cfc28bac905d4bea2391e
Wilhelm Thieme <wjthieme@gmail.com>
2023-01-09 10:34:33 +0100

    Big bang update and rename

---
+++ api/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import { RegisterRoutes } from "./modules/routes.gen.js.js";
+import { RegisterLogger } from "./modules/log.js";
+import { RegisterRequestParser } from "./modules/request.js";
+import { RegisterErrorCatcher } from "./modules/error.js";
+import { RegisterDocs } from "./modules/docs.js";
+import { RegisterSecurityMiddleware } from "./modules/security.js";
+
+await mongoose.connect(process.env.MONGO_URL ?? "");
+const app: Application = express();
+const middlewares = [
+    RegisterLogger, 
+    RegisterRequestParser, 
+    RegisterSecurityMiddleware, 
+    RegisterRoutes,
+    RegisterDocs,
+    RegisterErrorCatcher
+];
+middlewares.forEach(x => x(app));
+app.listen(process.env.PORT);
---
+++ core/src/model/trolley.ts
@@ -0,0 +1,21 @@
+import { JTDSchemaType } from "ajv/dist/jtd";
+
+export interface IAccountTrolleyWidgetRequest {
+    email: string;
+}
+
+export const AccountTrolleyWidgetRequestSchema: JTDSchemaType<IAccountTrolleyWidgetRequest> = {
+    properties: {
+        email: { type: "string" }
+    }
+};
+
+export interface IAccountTrolleyWidgetResponse {
+    widgetLink: string;
+}
+
+export const AccountTrolleyWidgetResponseSchema: JTDSchemaType<IAccountTrolleyWidgetResponse> = {
+    properties: {
+        widgetLink: { type: "string" }
+    }
+};
